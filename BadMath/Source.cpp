684ac0d1e3eadb5fb13549e7135866177b9921f0
Jayden C <jaydencampbell590@gmail.com>
2021-04-02 17:33:02 -0400

    In Line 16 (Original), Replaced 'n2' with 'n3'.
    In Line 7 (Original), Enclosed 'i1', 'i2', and 'i3' in a set of parenthesis.
    
    Moved lines 5-8 (Original) to lines 24-27 (New), and added a function prototype in line 5.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,10 +2,7 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
-{
-	return i1 + i2 + i3 / 3.0f;
-}
+float average(int, int, int);
 
 int main()
 {
@@ -13,11 +10,18 @@ int main()
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
 	cout << setprecision(1) << fixed << "The average is " << a << endl;
 
+	cin.ignore(100, '\n');
+	cin.get();
 	return 0;
+}
+
+float average(int i1, int i2, int i3)
+{
+	return (i1 + i2 + i3) / 3.0f;
 }
